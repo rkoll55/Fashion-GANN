b545a91b58d9ade32b2a7f2f4cb48db112747fe8
rkoll55 <rohankollambalath@gmail.com>
2023-02-28 00:35:34 +1000

    started making the generator

--- gan.py
+++ gan.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Conv2D,Dropout,Dense,Flatten,Conv2DTranspose,BatchNormalization,LeakyReLU, Reshape
+
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu,True)
@@ -31,3 +34,17 @@ ds = ds.cache()
 ds = ds.shuffle(50000)
 ds = ds.batch(128)
 ds = ds.prefetch(64)
+print(ds.as_numpy_iterator().next().shape) # should be 128 images, 28x28 with 1 for grayscale 
+
+# sequential API for generator and discriminator
+def build_generator():
+    model = Sequential()
+    # sequential used to build a stack of layers
+    model.add(Dense(7*7*128, input_dim=128))
+    # 128 input values mapped to high dimentional space. Makes 7*7*128 feature map
+    model.add(LeakyReLU(0.2))
+    # Helps identify non linearities in the dataset and smooths them out with sigmoid like function.  
+    model.add(Reshape((7,7,128)))
+    # takes the dense layer and generates the 7*7 image with 128 layer
+
+    return model
